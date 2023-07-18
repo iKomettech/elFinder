79d9e78a2efe7dc9532fa761ca6b9c5f09d9ce7a
iKOMET <ganesh@ikomet.com>
2023-07-18 11:34:58 +0530

    root path changed

--- nodejs/node-connecter/index.js
+++ nodejs/node-connecter/index.js
@@ -7,7 +7,7 @@ const roots = [
   {
     driver: elFinder.LocalFileStorage,
     URL: 'https://files.flowdev.pubskoler.com/', //Required
-    path: '/var/www/html/FileServer/JournalDemo/PIH1131883/', //Required
+    path: '/var/www/html/jenkins/', //Required
     permissions: { read: 1, write: 1, lock: 0 },
   }
 ];
