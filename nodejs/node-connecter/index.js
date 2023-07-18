67cda71045e4e21cb31c0efa58032ab0a05aebfc
iKOMET <ganesh@ikomet.com>
2023-07-18 12:19:39 +0530

    api fix

--- nodejs/node-connecter/index.js
+++ nodejs/node-connecter/index.js
@@ -8,7 +8,7 @@ const roots = [
     driver: elFinder.LocalFileStorage,
     URL: 'https://files.flowdev.pubskoler.com/', //Required
     path: '/var/www/html/FileServer/JournalDemo/PIH1131883/', //Required
-    permissions: { read: 1, write: 1, lock: 0 },
+    permissions: { read: 1, write: 1, locked: 0 },
   }
 ];
 
